85894c3670a820290823167bf6272fd84135b729
matsumotory <matsumotory@pepabo.com>
2017-01-30 10:35:34 +0000

    Apply clang-format

--- src/mrb_namespace.c
+++ src/mrb_namespace.c
@@ -8,6 +8,7 @@
 
 #define _GNU_SOURCE 1
 
+// clang-format off
 #include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
@@ -28,6 +29,7 @@
 #include <mruby/proc.h>
 
 #include "mrb_namespace.h"
+// clang-format on
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
@@ -37,13 +39,15 @@
 #define SYS_setns 308
 #endif
 
-static int setns(int fd, int nstype) {
+static int setns(int fd, int nstype)
+{
   long ret = syscall(SYS_setns, fd, nstype);
   return (int)ret;
 }
 #endif
 
-static int mrb_namespace_setns(mrb_state *mrb, int fd, int nstype) {
+static int mrb_namespace_setns(mrb_state *mrb, int fd, int nstype)
+{
   return setns(fd, nstype);
 }
 
@@ -55,7 +59,7 @@ static mrb_value mrb_namespace_unshare(mrb_state *mrb, mrb_value self)
   mrb_get_args(mrb, "i", &arg);
   unshare_flags = (int)arg;
 
-  return mrb_fixnum_value((mrb_int) unshare(unshare_flags));
+  return mrb_fixnum_value((mrb_int)unshare(unshare_flags));
 }
 
 static mrb_value mrb_namespace_setns_by_fd(mrb_state *mrb, mrb_value self)
@@ -71,7 +75,8 @@ static mrb_value mrb_namespace_setns_by_fd(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(ret);
 }
 
-static int mrb_namespace_pid_to_nsfile(mrb_state *mrb, char** procpath, pid_t _pid, int flag) {
+static int mrb_namespace_pid_to_nsfile(mrb_state *mrb, char **procpath, pid_t _pid, int flag)
+{
   const char *procpath_fmt = "/proc/%i/ns/%s";
   int ret = 0;
   int pid = (int)_pid;
@@ -116,34 +121,22 @@ static mrb_value mrb_namespace_setns_by_pid(mrb_state *mrb, mrb_value self)
 
 #ifdef CLONE_NEWCGROUP
   int namespaces[8] = {
-    CLONE_NEWNS,
-    CLONE_NEWUTS,
-    CLONE_NEWIPC,
-    CLONE_NEWUSER,
-    CLONE_NEWPID,
-    CLONE_NEWNET,
-    CLONE_NEWCGROUP,
-    0,
+      CLONE_NEWNS, CLONE_NEWUTS, CLONE_NEWIPC, CLONE_NEWUSER, CLONE_NEWPID, CLONE_NEWNET, CLONE_NEWCGROUP, 0,
   };
 #else
   int namespaces[7] = {
-    CLONE_NEWNS,
-    CLONE_NEWUTS,
-    CLONE_NEWIPC,
-    CLONE_NEWUSER,
-    CLONE_NEWPID,
-    CLONE_NEWNET,
-    0,
+      CLONE_NEWNS, CLONE_NEWUTS, CLONE_NEWIPC, CLONE_NEWUSER, CLONE_NEWPID, CLONE_NEWNET, 0,
   };
 #endif
 
   mrb_get_args(mrb, "ii", &pid, &nsflag);
 
-  for(int i=0; namespaces[i]; ++i) {
+  for (int i = 0; namespaces[i]; ++i) {
     int curns = nsflag & namespaces[i];
-    if(!curns) continue;
+    if (!curns)
+      continue;
 
-    if(mrb_namespace_pid_to_nsfile(mrb, &procpath, (pid_t)pid, curns) < 0) {
+    if (mrb_namespace_pid_to_nsfile(mrb, &procpath, (pid_t)pid, curns) < 0) {
       mrb_raise(mrb, E_RUNTIME_ERROR, "ns file detection failed");
     }
     fileno = open(procpath, O_RDONLY);
@@ -166,25 +159,25 @@ static mrb_value mrb_namespace_persist_ns(mrb_state *mrb, mrb_value self)
 
   mrb_get_args(mrb, "iiz", &pid, &flag, &dest);
 
-  if(!pid) {
+  if (!pid) {
     pid = (mrb_int)getpid();
   }
 
   char *procpath;
-  if(mrb_namespace_pid_to_nsfile(mrb, &procpath, (pid_t)pid, (int)flag) < 0) {
+  if (mrb_namespace_pid_to_nsfile(mrb, &procpath, (pid_t)pid, (int)flag) < 0) {
     mrb_raise(mrb, E_RUNTIME_ERROR, "ns file detection failed");
   }
 
   int dest_fd;
-  if((dest_fd = open(dest, O_WRONLY|O_CREAT, 0660)) < 0){
+  if ((dest_fd = open(dest, O_WRONLY | O_CREAT, 0660)) < 0) {
     mrb_sys_fail(mrb, "open dest file failed");
   }
-  if(futimens(dest_fd, NULL) < 0){
+  if (futimens(dest_fd, NULL) < 0) {
     mrb_sys_fail(mrb, "futimens failed");
   }
   close(dest_fd);
 
-  if(mount(procpath, dest, "none", MS_BIND, NULL) < 0){
+  if (mount(procpath, dest, "none", MS_BIND, NULL) < 0) {
     mrb_sys_fail(mrb, "mount failed - cannot bind ns file to other location");
   }
   return mrb_fixnum_value(0);
@@ -194,11 +187,11 @@ struct mrb_clone_params {
   mrb_state *mrb;
   mrb_value block;
 };
-static volatile struct mrb_clone_params* clone_params = NULL;
+static volatile struct mrb_clone_params *clone_params = NULL;
 
 static int mrb_clone_childfunc(void *params)
 {
-  if(!clone_params) {
+  if (!clone_params) {
     printf("[BUG] No clone params...\n");
     abort();
   }
@@ -236,12 +229,8 @@ static mrb_value mrb_namespace_clone(mrb_state *mrb, mrb_value self)
     stack_top = stack + STACK_SIZE;
 
     clone_params = p;
-    pid = clone(
-      mrb_clone_childfunc,
-      stack_top,
-      SIGCHLD|flag,
-      NULL);
-    if(pid < 0) {
+    pid = clone(mrb_clone_childfunc, stack_top, SIGCHLD | flag, NULL);
+    if (pid < 0) {
       perror("clone");
       mrb_sys_fail(mrb, "clone failed");
     }
@@ -261,32 +250,32 @@ void mrb_mruby_linux_namespace_gem_init(mrb_state *mrb)
   mrb_define_class_method(mrb, namespace, "setns_by_fd", mrb_namespace_setns_by_fd, MRB_ARGS_REQ(2));
   mrb_define_class_method(mrb, namespace, "setns_by_pid", mrb_namespace_setns_by_pid, MRB_ARGS_REQ(2));
   mrb_define_class_method(mrb, namespace, "persist_ns", mrb_namespace_persist_ns, MRB_ARGS_REQ(3));
-  mrb_define_class_method(mrb, namespace, "clone",  mrb_namespace_clone,  (MRB_ARGS_REQ(1)|MRB_ARGS_BLOCK()));
+  mrb_define_class_method(mrb, namespace, "clone", mrb_namespace_clone, (MRB_ARGS_REQ(1) | MRB_ARGS_BLOCK()));
 
-  mrb_define_const(mrb, namespace, "CLONE_VM",             mrb_fixnum_value(CLONE_VM));
-  mrb_define_const(mrb, namespace, "CLONE_FS",             mrb_fixnum_value(CLONE_FS));
-  mrb_define_const(mrb, namespace, "CLONE_FILES",          mrb_fixnum_value(CLONE_FILES));
-  mrb_define_const(mrb, namespace, "CLONE_SIGHAND",        mrb_fixnum_value(CLONE_SIGHAND));
-  mrb_define_const(mrb, namespace, "CLONE_PTRACE",         mrb_fixnum_value(CLONE_PTRACE));
-  mrb_define_const(mrb, namespace, "CLONE_VFORK",          mrb_fixnum_value(CLONE_VFORK));
-  mrb_define_const(mrb, namespace, "CLONE_PARENT",         mrb_fixnum_value(CLONE_PARENT));
-  mrb_define_const(mrb, namespace, "CLONE_THREAD",         mrb_fixnum_value(CLONE_THREAD));
-  mrb_define_const(mrb, namespace, "CLONE_NEWNS",          mrb_fixnum_value(CLONE_NEWNS));
-  mrb_define_const(mrb, namespace, "CLONE_SYSVSEM",        mrb_fixnum_value(CLONE_SYSVSEM));
-  mrb_define_const(mrb, namespace, "CLONE_SETTLS",         mrb_fixnum_value(CLONE_SETTLS));
-  mrb_define_const(mrb, namespace, "CLONE_PARENT_SETTID",  mrb_fixnum_value(CLONE_PARENT_SETTID));
+  mrb_define_const(mrb, namespace, "CLONE_VM", mrb_fixnum_value(CLONE_VM));
+  mrb_define_const(mrb, namespace, "CLONE_FS", mrb_fixnum_value(CLONE_FS));
+  mrb_define_const(mrb, namespace, "CLONE_FILES", mrb_fixnum_value(CLONE_FILES));
+  mrb_define_const(mrb, namespace, "CLONE_SIGHAND", mrb_fixnum_value(CLONE_SIGHAND));
+  mrb_define_const(mrb, namespace, "CLONE_PTRACE", mrb_fixnum_value(CLONE_PTRACE));
+  mrb_define_const(mrb, namespace, "CLONE_VFORK", mrb_fixnum_value(CLONE_VFORK));
+  mrb_define_const(mrb, namespace, "CLONE_PARENT", mrb_fixnum_value(CLONE_PARENT));
+  mrb_define_const(mrb, namespace, "CLONE_THREAD", mrb_fixnum_value(CLONE_THREAD));
+  mrb_define_const(mrb, namespace, "CLONE_NEWNS", mrb_fixnum_value(CLONE_NEWNS));
+  mrb_define_const(mrb, namespace, "CLONE_SYSVSEM", mrb_fixnum_value(CLONE_SYSVSEM));
+  mrb_define_const(mrb, namespace, "CLONE_SETTLS", mrb_fixnum_value(CLONE_SETTLS));
+  mrb_define_const(mrb, namespace, "CLONE_PARENT_SETTID", mrb_fixnum_value(CLONE_PARENT_SETTID));
   mrb_define_const(mrb, namespace, "CLONE_CHILD_CLEARTID", mrb_fixnum_value(CLONE_CHILD_CLEARTID));
-  mrb_define_const(mrb, namespace, "CLONE_DETACHED",       mrb_fixnum_value(CLONE_DETACHED));
-  mrb_define_const(mrb, namespace, "CLONE_UNTRACED",       mrb_fixnum_value(CLONE_UNTRACED));
-  mrb_define_const(mrb, namespace, "CLONE_CHILD_SETTID",   mrb_fixnum_value(CLONE_CHILD_SETTID));
-  mrb_define_const(mrb, namespace, "CLONE_NEWUTS",         mrb_fixnum_value(CLONE_NEWUTS));
-  mrb_define_const(mrb, namespace, "CLONE_NEWIPC",         mrb_fixnum_value(CLONE_NEWIPC));
-  mrb_define_const(mrb, namespace, "CLONE_NEWUSER",        mrb_fixnum_value(CLONE_NEWUSER));
-  mrb_define_const(mrb, namespace, "CLONE_NEWPID",         mrb_fixnum_value(CLONE_NEWPID));
-  mrb_define_const(mrb, namespace, "CLONE_NEWNET",         mrb_fixnum_value(CLONE_NEWNET));
-  mrb_define_const(mrb, namespace, "CLONE_IO",             mrb_fixnum_value(CLONE_IO));
+  mrb_define_const(mrb, namespace, "CLONE_DETACHED", mrb_fixnum_value(CLONE_DETACHED));
+  mrb_define_const(mrb, namespace, "CLONE_UNTRACED", mrb_fixnum_value(CLONE_UNTRACED));
+  mrb_define_const(mrb, namespace, "CLONE_CHILD_SETTID", mrb_fixnum_value(CLONE_CHILD_SETTID));
+  mrb_define_const(mrb, namespace, "CLONE_NEWUTS", mrb_fixnum_value(CLONE_NEWUTS));
+  mrb_define_const(mrb, namespace, "CLONE_NEWIPC", mrb_fixnum_value(CLONE_NEWIPC));
+  mrb_define_const(mrb, namespace, "CLONE_NEWUSER", mrb_fixnum_value(CLONE_NEWUSER));
+  mrb_define_const(mrb, namespace, "CLONE_NEWPID", mrb_fixnum_value(CLONE_NEWPID));
+  mrb_define_const(mrb, namespace, "CLONE_NEWNET", mrb_fixnum_value(CLONE_NEWNET));
+  mrb_define_const(mrb, namespace, "CLONE_IO", mrb_fixnum_value(CLONE_IO));
 #ifdef CLONE_NEWCGROUP
-  mrb_define_const(mrb, namespace, "CLONE_NEWCGROUP",      mrb_fixnum_value(CLONE_NEWCGROUP));
+  mrb_define_const(mrb, namespace, "CLONE_NEWCGROUP", mrb_fixnum_value(CLONE_NEWCGROUP));
 #endif
 
   DONE;
