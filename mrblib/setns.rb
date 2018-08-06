34e7354767d550feded296400e3a9dad2ec71992
Uchio KONDO <udzura@pepabo.com>
2018-08-06 16:20:38 +0900

    Implement nsenter with block

--- mrblib/setns.rb
+++ mrblib/setns.rb
@@ -1,5 +1,9 @@
 module Namespace
-  def self.setns(flag, options)
+  def self.setns(flag, options, &blk)
+    if blk
+      return nsenter(flag, options, &blk)
+    end
+
     fd = options[:fd]
     pid = options[:pid]
 
@@ -10,5 +14,27 @@ module Namespace
     else
       raise ArgumentError, "Options :fd or :pid must be specified"
     end
+  end
+
+  def self.nsenter(flag, options, &blk)
+    unless Object.const_get(:Process)
+      raise NotImplementedError, "Namespace.nsenter depends on mruby-process"
+    end
+
+    pid = Process.fork do
+      begin
+        ::Namespace.setns(flag, options)
+        blk.call
+      rescue => e
+        raise e
+        exit(127)
+      end
+    end
+
+    pid, s = Process.waitpid2 pid
+    unless s.success?
+      raise "nsenter process failed in some reason: PID=#{pid}, #{s.inspect}"
+    end
+    return s
   end
 end
